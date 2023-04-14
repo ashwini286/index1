b108b4d60a307d8ba21e6e7ff19084c3930f45ad
Ashwini Sharma <83415972+ashwini286@users.noreply.github.com>
2023-04-14 21:16:01 +0530

    Add files via upload
    all file here.

---
+++ jsquery/jspraties.js
@@ -0,0 +1,17 @@
+// element selector
+// $('#hello').click("i am here");
+// $().click(function(){
+//     $("#hello").css({"color":"blue","background-color":"red"})
+// })
+// $('#hello').click(function(){
+//     console.log("you clicked me")
+// })
+// $("#hello").dblclick(function(){
+//     console.log("i am doble click")
+//     $("#hello").css({"color":"blue","background-color":"red"})
+// });
+// $('p').click(function(){
+//     console.log("you clicked me")
+// })
+
+
